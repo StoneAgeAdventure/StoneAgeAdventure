49786d60b8fca942cbce5b0e8ff6f2d43b9efebe
eurekatea <eurekatea701@gmail.com>
2022-05-22 17:01:26 +0800

    improve bgm

--- singleton/music.gd
+++ singleton/music.gd
@@ -3,6 +3,4 @@ extends AudioStreamPlayer
 onready var bgm_main_world = preload("res://sounds/maoudamashii/bgm_maoudamashii_acoustic44.mp3")
 
 func _ready():
-	self.stream = bgm_main_world
-	self.play()
-	self.volume_db = -10
+	pass
