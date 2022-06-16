13b9b972b243df2fdf656bbedf6c3382292f4dbc
lemon37564 <stonydragon0701@gmail.com>
2022-06-16 21:11:29 +0800

    improve debug scene

--- singleton/debug.gd
+++ singleton/debug.gd
@@ -6,5 +6,6 @@ onready var _label = $Panel/Label
 func _process(_delta):
 	if debug: 
 		var fps := Engine.get_frames_per_second() as int
+		var mem := OS.get_static_memory_usage() / 1024.0 / 1024.0
 		var objects := get_tree().get_node_count()
-		_label.text = "Objects: %d\nFPS: %d" % [objects, fps]
+		_label.text = "Objects: %d\nMem: %.1fM\nFPS: %d" % [objects, mem, fps]
